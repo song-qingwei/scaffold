45d9ca6644d8ee87f5357d539df1febc7f8f324e
songqingwei <songqingwei@xiaomi.com>
2021-03-09 11:00:39 +0800

    feat(scaffold): shell
    修改shell脚本

--- src/main/java/com/example/scaffold/ScaffoldApplication.java
+++ src/main/java/com/example/scaffold/ScaffoldApplication.java
@@ -3,6 +3,9 @@ package com.example.scaffold;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author admin
+ */
 @SpringBootApplication
 public class ScaffoldApplication {
 
